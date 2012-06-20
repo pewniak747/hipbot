078879cc36f7190bee70a0460bc7d300359ad685
Tomasz Pewiński <pewniak747@gmail.com>
2012-06-20 21:06:43 +0200

    global & private messages

--- lib/hipbot.rb
+++ lib/hipbot.rb
@@ -1,44 +1,81 @@
-class Hipbot
-  attr_accessor :reactions
+module Hipbot
+class Bot
+  attr_accessor :reactions, :name
   def initialize
-    self.reactions = self.class.reactions 
+    self.reactions = []
+    self.class.reactions.each do |opts|
+      on(opts[0], opts[1], &opts[-1])
+    end
+    self.name = 'robot'
   end
 
-  def on regexp, &block
-    self.reactions << Reaction.new(regexp, block)    
+  def on regexp, options={}, &block
+    self.reactions << Reaction.new(self, regexp, options, block)
   end
 
   def tell message
-    matches = reactions.select { |r| r.match?(message) }
+    matches = matching_reactions(message)
     if matches.size == 1
       match = matches.first
       arguments = match.arguments_for(message)
       instance_exec(*arguments, &match.block)
     elsif matches.size > 1
       reply("I'm not sure what to do...")
-    else
-      reply("I don't understand \"#{message}\"")
+    elsif to_me?(message)
+      reply("I don't understand \"#{message.gsub(/^@#{name}\s*/, '')}\"")
     end
   end
 
+  def reply message
+  end
+
+  def to_me? message
+    message =~ /^@#{name}/
+  end
+
   class << self
-  def on regexp, &block
+  def on regexp, options={}, &block
     @reactions ||= []
-    @reactions << Reaction.new(regexp, block)
+    @reactions << [regexp, options, block]
   end
 
   def reactions
     @reactions || []
   end
   end
+
+  private
+
+  def matching_reactions message
+    reactions.select { |r| r.match?(message) }
+  end
 end
 
-class Hipbot::Reaction < Struct.new(:regexp, :block)
+class Reaction < Struct.new(:robot, :regexp, :options, :block)
   def match? message
-    regexp =~ message 
+    matches?(message) && (global? || robot.to_me?(message))
   end
 
   def arguments_for message
-    message.match(regexp)[1..-1]
+    process_message(message).match(regexp)[1..-1]
   end
+
+  def global?
+    options[:global]
+  end
+
+  private
+
+  def matches? message
+    regexp =~ process_message(message)
+  end
+
+  def process_message message
+    unless global?
+      message.gsub(/^@#{robot.name}\s*/, '')
+    else
+      message
+    end
+  end
+end
 end
--- spec/hipbot_spec.rb
+++ spec/hipbot_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
-describe Hipbot do
+describe Hipbot::Bot do
   context "#on" do
     it "should reply to no arguments" do
       subject.on /^hello there$/ do
         reply('hi!')
       end
       subject.expects(:reply).with('hi!')
-      subject.tell('hello there')
+      subject.tell('@robot hello there')
     end
 
     it "should reply with one argument" do
@@ -15,7 +15,7 @@ describe Hipbot do
         reply("i know i'm #{adj}!")
       end
       subject.expects(:reply).with("i know i'm cool!")
-      subject.tell('you are cool, robot')
+      subject.tell('@robot you are cool, robot')
     end
 
     it "should reply with multipla arguments" do
@@ -23,7 +23,7 @@ describe Hipbot do
         reply("sent \"#{message}\" to #{recipient}")
       end
       subject.expects(:reply).with('sent "hello!" to robot@robots.org')
-      subject.tell('send "hello!" to robot@robots.org')
+      subject.tell('@robot send "hello!" to robot@robots.org')
     end
 
     it "should say when does not understand" do
@@ -31,26 +31,45 @@ describe Hipbot do
         reply('hi!')
       end
       subject.expects(:reply).with('I don\'t understand "hello robot!"')
-      subject.tell('hello robot!')
+      subject.tell('@robot hello robot!')
     end
 
     it "should say when multiple options match" do
       subject.on /hello there/ do; end
       subject.on /hello (.*)/ do; end
       subject.expects(:reply).with('I\'m not sure what to do...')
-      subject.tell('hello there')
+      subject.tell('@robot hello there')
+    end
+
+    it "should reply if callback is global" do
+      subject.on /^you are (.*)$/, global: true do |adj|
+        reply("i know i'm #{adj}!")
+      end
+      subject.expects(:reply).with("i know i'm cool!")
+      subject.tell('you are cool')
+    end
+
+    it "should not reply if callback not global" do
+      subject.on /^you are (.*)$/ do |adj|
+        reply("i know i'm #{adj}!")
+      end
+      subject.expects(:reply).never
+      subject.tell('you are cool')
     end
   end
 end
 
 describe "Custom Hipbot" do
   before :all do
-    class MyHipbot < Hipbot
+    class MyHipbot < Hipbot::Bot
       on /^hello hipbot!$/ do
         reply("hello!")
       end
       on /you're (.*), robot/ do |adj|
         reply("I know I'm #{adj}")
+      end
+      on /hi everyone!/, global: true do
+        reply('hello!')
       end
     end
   end
@@ -58,11 +77,16 @@ describe "Custom Hipbot" do
 
   it "should reply to hello" do
     subject.expects(:reply).with('hello!')
-    subject.tell('hello hipbot!')
+    subject.tell('@robot hello hipbot!')
   end
 
   it "should reply with argument" do
     subject.expects(:reply).with("I know I'm cool")
-    subject.tell("you're cool, robot")
+    subject.tell("@robot you're cool, robot")
+  end
+
+  it "should reply to global message" do
+    subject.expects(:reply).with("hello!")
+    subject.tell("hi everyone!")
   end
 end
