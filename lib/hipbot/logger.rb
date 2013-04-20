6f801b03ea26e42a43a6259266ed0e2532839b3d
Tomasz Pewiński <pewniak747@gmail.com>
2013-04-20 20:42:38 +0200

    print log levels

--- lib/hipbot/logger.rb
+++ lib/hipbot/logger.rb
@@ -4,7 +4,8 @@ module Hipbot
   class Logger < ::Logger
     def add(severity, message = nil, progname = nil, &block)
       msg = message || (block_given? and block.call) || progname
-      super(severity, "[#{severity}][#{Time.now}] #{msg}")
+      severity_name = { 0 => "DEBUG", 1 => "INFO", 2 => "WARN", 3 => "ERROR", 4 => "FATAL", 5 => "UNKNOWN" }[severity]
+      super(severity, "[#{severity_name}][#{Time.now}] #{msg}")
     end
   end
 
