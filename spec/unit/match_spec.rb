5fb3b0ef0560867f7125bc8490ae229b3661c32e
Tomasz Pewiński <pewniak747@gmail.com>
2013-09-13 23:04:30 +0200

    initial specs for matching class

---
+++ spec/unit/match_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+require_relative '../../lib/hipbot/match.rb'
+
+describe Hipbot::Match do
+  subject { described_class.new(reaction, message) }
+
+  let(:message) { stub(for?: true, body: 'test message') }
+  let(:reaction) { stub(global?: false, anywhere?: true, anything?: false, from_all?: true, regexps: [/.*/], condition: proc { true }) }
+
+  before do
+    Hipbot.stubs(:user)
+  end
+
+  describe "#matches?" do
+    its(:matches?) { should be_true }
+
+    describe "specific regexp" do
+      describe "matching the message body" do
+        before do
+          message.stubs(body: 'test message')
+          reaction.stubs(regexps: [/\Atest/])
+        end
+
+        its(:matches?) { should be_true }
+      end
+
+      describe "not matching message body" do
+        before do
+          message.stubs(body: 'test message')
+          reaction.stubs(regexps: [/\Arandom/])
+        end
+
+        its(:matches?) { should be_false }
+      end
+    end
+
+    describe "multiple regexps" do
+      describe "matching message body" do
+        before do
+          message.stubs(body: 'test message')
+          reaction.stubs(regexps: [/\Awat/, /\Atest/])
+        end
+
+        its(:matches?) { should be_true }
+      end
+
+      describe "not matching message body" do
+        before do
+          message.stubs(body: 'test message')
+          reaction.stubs(regexps: [/\Awat/, /\Arandom/])
+        end
+
+        its(:matches?) { should be_false }
+      end
+    end
+  end
+end
