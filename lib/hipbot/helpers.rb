37065d8a5ef32f600bd8aaa954482e3de65a62f3
Bartosz Kopiński <bartosz.kopinski@netguru.pl>
2013-09-20 06:09:36 +0200

    ref http helpers

--- lib/hipbot/helpers.rb
+++ lib/hipbot/helpers.rb
@@ -1,46 +1,10 @@
 module Hipbot
   module Helpers
-    [:get, :post, :put, :delete].each do |http_verb|
-      define_method http_verb do |url, query = {}, &block|
-        Hipbot.logger.info("HTTP-REQUEST: #{url} #{query}")
-        query.merge!({ :head => {'accept-encoding' => 'gzip, compressed'} })
-        conn = ::EM::HttpRequest.new(url, :connect_timeout => 5, :inactivity_timeout => 10)
-        http = conn.send(http_verb, :query => query)
-        http.callback do
-          begin
-            response = HttpResponse.new(http)
-            Hipbot.logger.info("HTTP-RESPONSE: #{response}")
-            block.call(response)
-          rescue => e
-            Hipbot.logger.error(e)
-            instance_exec(e, &Hipbot.error_handler)
-          end
-        end if block.present?
-
-        http.errback do
-          Hipbot.logger.error("HTTP-RESPONSE-ERROR: #{url}")
-        end
+    [:get, :post, :put, :delete].each do |method|
+      define_method method do |url, query = {}, &block|
+        Http::Request.new(url, query, method).call(&block)
       end
-
-      module_function http_verb
-    end
-
-    class HttpResponse < Struct.new(:raw_response)
-      def body
-        raw_response.response
-      end
-
-      def code
-        raw_response.response_header.status
-      end
-
-      def headers
-        raw_response.response_header
-      end
-
-      def json
-        @json ||= JSON.parse(body)
-      end
+      module_function method
     end
   end
 end
---
+++ lib/hipbot/http.rb
@@ -0,0 +1,61 @@
+module Hipbot
+  module Http
+    class Request < Struct.new(:url, :query, :method)
+      DEFAULT_HEADERS     = { 'accept-encoding' => 'gzip, compressed' }.freeze
+      CONNECTION_SETTINGS = { connect_timeout: 5, inactivity_timeout: 10 }.freeze
+      ERROR_CALLBACK      = ->{ Hipbot.logger.error("HTTP-RESPONSE-ERROR: #{url}") }
+
+      def initialize *args
+        super
+        Hipbot.logger.info("HTTP-REQUEST: #{url} #{query}")
+      end
+
+      def call &success_block
+        http.errback(&ERROR_CALLBACK)
+        http.callback do
+          success(&success_block)
+        end if success_block.present?
+      end
+
+      protected
+
+      def success
+        yield Http::Response.new(http)
+      rescue => e
+        Hipbot.logger.error(e)
+        instance_exec(e, &Hipbot.error_handler)
+      end
+
+      def http
+        @http ||= connection.send(method, query: query.merge(head: DEFAULT_HEADERS))
+      end
+
+      def connection
+        EM::HttpRequest.new(url, CONNECTION_SETTINGS)
+      end
+    end
+
+    class Response < Struct.new(:raw_response)
+      def initialize *args
+        super
+        Hipbot.logger.info("HTTP-RESPONSE: #{headers}")
+      end
+
+      def body
+        raw_response.response
+      end
+
+      def code
+        raw_response.response_header.status
+      end
+
+      def headers
+        raw_response.response_header
+      end
+
+      def json
+        @json ||= JSON.parse(body)
+      end
+    end
+  end
+end
