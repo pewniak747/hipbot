143b657c57811b28cd6be3a328bb7d0a81b02618
Bartosz Kopiński <bartosz.kopinski@netguru.pl>
2013-09-26 05:15:39 +0200

    handle lobby presence

--- lib/hipbot/callbacks/presence.rb
+++ lib/hipbot/callbacks/presence.rb
@@ -1,27 +1,45 @@
 module Hipbot
   module Callbacks
     class Presence < Base
-      def initialize room_id, user_name, presence
-        with_sender(room_id, user_name) do |room, user|
-          if user_left?(presence)
-            if user.myself?
-              room.destroy
-            elsif !user.nil?
-              room.users.delete(user)
-            end
-          elsif user_joined?(presence, room, user)
-            room.users << user
-          end
+      attr_accessor :presence
+
+      def initialize from_id, user_name, presence
+        self.presence = presence.to_s
+
+        if user_name.nil?
+          lobby_presence(from_id)
+        else
+          room_presence(from_id, user_name)
         end
       end
 
       protected
 
-      def user_joined? presence, room, user
-        presence.empty? && !room.users.include?(user)
+      def lobby_presence user_id
+        with_user(user_id) do |user|
+          user.update_attribute(:is_online, online?)
+        end
       end
 
-      def user_left? presence
+      def room_presence room_id, user_name
+        with_sender(room_id, user_name) do |room, user|
+          if offline?
+            if user.myself?
+              room.destroy
+            elsif !user.nil?
+              room.users.delete(user)
+            end
+          elsif online? && !room.users.include?(user)
+            room.users << user
+          end
+        end
+      end
+
+      def online?
+        presence.empty?
+      end
+
+      def offline?
         presence == 'unavailable'
       end
     end
