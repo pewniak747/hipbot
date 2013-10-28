56e200468fc9b0849449e580e80991ff7353f487
Bartosz Kopiński <bartosz.kopinski@netguru.pl>
2013-10-28 22:51:43 +0100

    fixing empty sender bug

--- lib/hipbot/callbacks/base.rb
+++ lib/hipbot/callbacks/base.rb
@@ -3,15 +3,15 @@ module Hipbot
     class Base
       protected
 
-      def with_sender room_id, user_id
+      def with_sender room_id, user_name
         room = Room.where(id: room_id).first
-        with_user(user_id) do |user|
+        with_user(user_name) do |user|
           yield room, user
         end unless room.nil?
       end
 
-      def with_user user_id
-        yield User.find_or_initialize_by(id: user_id)
+      def with_user user_name
+        yield User.find_or_initialize_by(name: user_name)
       end
     end
   end
