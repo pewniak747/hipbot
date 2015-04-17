2ef9dc8f705ac4ecaf8a839be9325daaff5fceb8
Bartosz Kopiński <bartosz.kopinski@netguru.pl>
2015-04-17 13:14:40 +0200

    Value validation

--- lib/hipbot/callbacks/base.rb
+++ lib/hipbot/callbacks/base.rb
@@ -4,11 +4,17 @@ module Hipbot
       protected
 
       def with_room params
-        yield Room.find_or_create_by(params)
+        yield Room.find_or_create_by(params) if valid_params?(params)
       end
 
       def with_user params
-        yield User.find_or_create_by(params)
+        yield User.find_or_create_by(params) if valid_params?(params)
+      end
+
+      private
+
+      def valid_params? params
+        params.any?{ |_, v| !v.nil? && v.present? }
       end
     end
   end
