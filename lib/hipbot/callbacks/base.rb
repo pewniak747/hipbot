d4936ab9ec36bbd81b04be747c00fab6a994cfe5
Bartosz Kopiński <bartosz.kopinski@netguru.pl>
2015-04-24 20:33:05 +0200

    Finding user by name (hipchat) or mention (slack)

--- lib/hipbot/callbacks/base.rb
+++ lib/hipbot/callbacks/base.rb
@@ -11,10 +11,22 @@ module Hipbot
         yield User.find_or_create_by(params) if valid_params?(params)
       end
 
+      def with_user_by_name_or_mention name_or_mention
+        return if !valid_value?(name_or_mention)
+
+        yield User.where(mention: name_or_mention).first ||
+          User.where(name: name_or_mention).first ||
+          User.create(name: name_or_mention, mention: name_or_mention)
+      end
+
       private
 
       def valid_params? params
-        params.any?{ |_, v| !v.nil? && v.present? }
+        params.any?{ |_, v| valid_value?(v) }
+      end
+
+      def valid_value? value
+        !value.nil? && value.present?
       end
     end
   end
