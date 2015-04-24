6d32b630fc615c10d5762bcd600170fb7ea3e07a
Bartosz Kopiński <bartosz.kopinski@netguru.pl>
2015-04-24 20:33:30 +0200

    Update room message callback

--- lib/hipbot/callbacks/room_message.rb
+++ lib/hipbot/callbacks/room_message.rb
@@ -1,20 +1,14 @@
 module Hipbot
   module Callbacks
     class RoomMessage < Message
-      def initialize room_id, user_name, message_body, topic
+      def initialize room_id, user_name_or_mention, message_body
         with_room(id: room_id) do |room|
-          update_topic(room, topic)
-          with_user(name: user_name) do |user|
+          with_user_by_name_or_mention(user_name_or_mention) do |user|
             return if ignore_message?(user, message_body)
+
             Hipbot.react(user, room, message_body)
           end
         end
-      end
-
-      protected
-
-      def update_topic room, topic
-        room.update_attribute(:topic, topic) unless topic.empty?
       end
     end
   end
